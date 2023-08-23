cadd13854d1c9ca3a0ff5f2fa6f82d5d8ea31774
JoshuaIsADev <JoshuaIsADev@gmail.com>
2023-08-22 21:48:24 -0700

    added a form that handles submit to the ToDo component

--- src/components/ToDo.js
+++ src/components/ToDo.js
@@ -1,3 +1,21 @@
+import { useState } from 'react';
+
+const initialItems = [
+  {
+    id: 1,
+    description: 'Finish Anteroom v2 design',
+    quantity: '',
+    completed: true,
+  },
+  {
+    id: 2,
+    description: 'Convert the Anteroom into a React app',
+    quantity: '',
+    completed: false,
+  },
+  { id: 3, description: 'Pack passport', quantity: 1, completed: false },
+];
+
 export default function ToDo() {
   return (
     <div>
@@ -14,14 +32,60 @@ function Logo() {
 }
 
 function Form() {
+  const [description, setDescription] = useState('');
+  const [quantity, setQuantity] = useState('');
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    if (!description) return;
+
+    const newItem = { description, quantity, completed: false, id: Date.now() };
+    console.log(newItem);
+
+    setDescription('');
+    setQuantity('');
+  }
+
   return (
-    <div className='form'>
+    <form className='form' onSubmit={handleSubmit}>
       <h5>Add An Item</h5>
-    </div>
+      <input
+        type='text'
+        placeholder=''
+        value={description}
+        onChange={(e) => setDescription(e.target.value)}
+      />
+      <input
+        type='number'
+        placeholder=''
+        value={quantity}
+        onChange={(e) => setQuantity(Number(e.target.value))}
+      />
+      <button>+</button>
+    </form>
   );
 }
+
 function ToDoList() {
-  return <div className='todo-list'>List</div>;
+  return (
+    <ul className='todo-list'>
+      {initialItems.map((item) => (
+        <Item item={item} key={item.id} />
+      ))}
+    </ul>
+  );
+}
+
+function Item({ item }) {
+  return (
+    <li>
+      <span style={item.completed ? { background: '#F6EA48' } : {}}>
+        {item.description} {item.quantity}
+      </span>
+      <button>&times;</button>
+    </li>
+  );
 }
 
 function Stats() {
